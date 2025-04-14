3652cf80e67eea66bd5d62e33465152db73bad0c
hao <ruanhao5056@gmail.com>
2025-04-14 18:11:10 +0800

    后端初始化

---
+++ src/main/java/com/hao/mainshihao/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.hao.mainshihao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hao.mainshihao.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *  
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
